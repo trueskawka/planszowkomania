2f526de4ff524a0f38ac75714281d05ac0dc60b5
Alicja Raszkowska <malavarena@gmail.com>
2016-03-19 12:29:22 +0100

    removing user from table

--- frontend/js/mainscript.js
+++ frontend/js/mainscript.js
@@ -9,6 +9,7 @@ $( document ).ready(function() {
   fetchUserTables();
 
 });
+
 // Ustawianie buttonów do zapisywania się do stołu
 var setupSignupButtons = function() {
 
@@ -23,7 +24,9 @@ var setupSignupButtons = function() {
       getUserDetails(function(data) {
         var imurl = "../files/images/" + data.image;
         var empty = $(parent).find(".empty-user").first()[0];
-        $(empty).css("background-image", "url("+imurl+")");
+        $(empty).css({"background-image": "url("+imurl+")",
+                      "opacity": ".4"});
+        $(empty).attr("data-userid", data.id);
         });
     });
 
@@ -35,6 +38,17 @@ var setupSignupButtons = function() {
     var gr = $(e.currentTarget);
     var parent = $(gr).parents('.boxy')[0];
     var or = $(parent).find('.btn-warning');
+
+    var tableid = $(parent).data("tableid");
+    tableid = "api/table/leave/" + tableid;
+
+    authorizedPost(tableid, null, function(){
+      getUserDetails(function(data) {
+        var userid = data.id;
+        $(parent).find("[data-userid='" + userid + "']").remove();
+        $(parent).find(".users").append('<div class="empty-user"></div>');
+        });
+    });
 
     or.removeClass("hidden");
     gr.addClass("hidden");
@@ -48,7 +62,6 @@ var fetchUserTables = function() {
   $.getJSON(serviceBase + 'api/table/all', function(data) {
 
     for (i = 0; i < data.length; i++) {
-      console.log(data[i]);
 
       var html = "";
       html += "<div class='boxy' data-tableid='" + data[i].id + "'>";
@@ -61,7 +74,11 @@ var fetchUserTables = function() {
       html += '<div class="users">';
 
       for (j = 0; j < data[i].participants.length; j++) {
-          html+= '<div class="user"><img src="../files/images/' + data[i].participants[j].image + '" class="usrimg" / ></div>';
+        if(data[i].participants[j].status == "Pending") {
+          html+= '<div class="user" data-userid="' + data[i].participants[j].id +'"><img src="../files/images/' + data[i].participants[j].image + '" class="usrimg" style="opacity: .4" / ></div>';
+        } else {
+          html+= '<div class="user" data-userid="' + data[i].participants[j].id +'"><img src="../files/images/' + data[i].participants[j].image + '" class="usrimg" / ></div>';
+        }
       }
 
       if (data[i].participants.length < data[i].usersRequired) {
