d9e4566077ae71ca289ec62d13ce6af5b6b5693e
kelostrada <kelostrada@gmail.com>
2016-03-19 11:00:53 +0100

    table/accept table/Reject user/details/{user_id} table/leave/{id}

---
+++ backend/Planszowkomania.API/Models/Front/TableAcceptUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planszowkomania.API.Models.Front
+{
+    public class TableAcceptUser
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public int TableId { get; set; }
+    }
+}
--- backend/Planszowkomania.API/Services/TableService.cs
+++ backend/Planszowkomania.API/Services/TableService.cs
@@ -37,5 +37,29 @@ namespace Planszowkomania.API.Services
             _context.Participations.Add(participation);
             _context.SaveChanges();
         }
+
+        public void HandleUser(string ownerId, string userId, int tableId, bool accept)
+        {
+            var participation = _context.Participations.FirstOrDefault(p => p.ParticipantId == userId && p.TableId == tableId && p.Table.OwnerId == ownerId);
+            if (participation == null)
+            {
+                throw new Exception("User didn't participate in table");
+            }
+
+            participation.Status = accept ? AcceptanceStatus.Accepted : AcceptanceStatus.Rejected;
+            _context.SaveChanges();
+        }
+
+        public void Leave(string userId, int tableId)
+        {
+            var participation = _context.Participations.FirstOrDefault(p => p.ParticipantId == userId && p.TableId == tableId);
+            if (participation == null)
+            {
+                throw new Exception("User didn't participate in table");
+            }
+
+            _context.Participations.Remove(participation);
+            _context.SaveChanges();
+        }
     }
 }
