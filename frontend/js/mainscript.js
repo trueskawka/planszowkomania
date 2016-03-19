d66d2902a45dd6fdd3818fe003745fb856a4f148
kelostrada <kelostrada@gmail.com>
2016-03-19 08:26:38 +0100

    cleaned up swapping buttons

--- frontend/js/mainscript.js
+++ frontend/js/mainscript.js
@@ -4,23 +4,9 @@ $( document ).ready(function() {
     $('#name').text(data.userName);
   });
 
+  setupSignupButtons();
 
-
-    $(".boxy").each(function(){
-      var or = $(this).find(".btn-warning");
-      var gr = $(this).find(".btn-success");
-
-      or.on("click", function(){
-        or.addClass("hidden");
-        gr.removeClass("hidden");
-      });
-
-      gr.on("click", function(){
-        or.removeClass("hidden");
-        gr.addClass("hidden");
-      });
-
-    });
+  fetchUserTables();
 
     /*$.ajax({
       beforeSend: function (xhr) {
@@ -42,38 +28,7 @@ $( document ).ready(function() {
       function(data){console.log(data)}
     })*/
 
-    var url = "http://vpn.geeksoft.pl:2140/api/table/all";
-
-    $.getJSON(url, function(data) {
-        console.log(data[0]);
-
-        for (i = 0; i < data.length; i++) {
-          var html = "";
-          html += "<div class='boxy'>";
-          html += '<div class="game-content"><img src="../files/images/' + data[i].game.image + '"/>';
-          html += '<div class="text-content"><div class="btn btn-warning btn-fab"><i class="fa fa-user-plus"></i></div><div class="btn btn-success btn-fab hidden"><i class="fa fa-check"></i></div><h4>' + data[i].game.name + '</h4><table>';
-          html += '<tr><td class="lead-cl">Miejsce: </td><td>' + data[i].localizationName + '</td></tr>';
-          html += '<tr><td class="lead-cl">Termin: </td><td>' + data[i].eventDate + '</td></tr>';
-          html += '<tr><td class="lead-cl">Poziom gry: </td><td>' + data[i].difficulty + '</td></tr>';
-          html += '<tr><td class="lead-cl">Poziom agresji: </td><td>' + data[i].aggresionLevel + '</td></tr></table>';
-          html += '<div class="users">';
-
-          for (j = 0; j < data[i].participants.length; j++) {
-              html+= '<div class="user"><img src="../files/images/' + data[i].participants[j].image + '" class="usrimg" / ></div>';
-          }
-
-          if (data[i].participants.length < 4) {
-            for (k = 0; k < 4 - data[i].participants.length; k++) {
-              html += '<div class="empty-user"></div>';
-            }
-          }
-
-          html += '</div></div></div></div>';
-
-          $(".rooms").append(html);
-        }
-
-    })
+    
 
     /*$.ajax({
       beforeSend: function (xhr) {
@@ -85,10 +40,6 @@ $( document ).ready(function() {
       function(data){console.log(data)}
 
     })*/
-
-
-
-
 
 });
 
@@ -124,9 +75,56 @@ var getUserDetails = function(callback) {
 // Ustawianie buttonów do zapisywania się do stołu
 var setupSignupButtons = function() {
 
+  $(".rooms").on('click', '.btn-warning', function(e) {
+    var or = $(e.currentTarget);
+    var parent = $(or).parents('.boxy')[0];
+    var gr = $(parent).find('.btn-success');
+
+    or.addClass("hidden");
+    gr.removeClass("hidden");
+  });
+
+  $(".rooms").on('click', '.btn-success', function(e) {
+    var gr = $(e.currentTarget);
+    var parent = $(gr).parents('.boxy')[0];
+    var or = $(parent).find('.btn-warning');
+    
+    or.removeClass("hidden");
+    gr.addClass("hidden");
+  });
+
 }
 
 // Pobieranie wszystkich stołów
 var fetchUserTables = function() {
 
+  $.getJSON(serviceBase + 'api/table/all', function(data) {
+
+    for (i = 0; i < data.length; i++) {
+      var html = "";
+      html += "<div class='boxy'>";
+      html += '<div class="game-content"><img src="../files/images/' + data[i].game.image + '"/>';
+      html += '<div class="text-content"><div class="btn btn-warning btn-fab"><i class="fa fa-user-plus"></i></div><div class="btn btn-success btn-fab hidden"><i class="fa fa-check"></i></div><h4>' + data[i].game.name + '</h4><table>';
+      html += '<tr><td class="lead-cl">Miejsce: </td><td>' + data[i].localizationName + '</td></tr>';
+      html += '<tr><td class="lead-cl">Termin: </td><td>' + data[i].eventDate + '</td></tr>';
+      html += '<tr><td class="lead-cl">Poziom gry: </td><td>' + data[i].difficulty + '</td></tr>';
+      html += '<tr><td class="lead-cl">Poziom agresji: </td><td>' + data[i].aggresionLevel + '</td></tr></table>';
+      html += '<div class="users">';
+
+      for (j = 0; j < data[i].participants.length; j++) {
+          html+= '<div class="user"><img src="../files/images/' + data[i].participants[j].image + '" class="usrimg" / ></div>';
+      }
+
+      if (data[i].participants.length < 4) {
+        for (k = 0; k < 4 - data[i].participants.length; k++) {
+          html += '<div class="empty-user"></div>';
+        }
+      }
+
+      html += '</div></div></div></div>';
+
+      $(".rooms").append(html);
+    }
+
+  })
 }
