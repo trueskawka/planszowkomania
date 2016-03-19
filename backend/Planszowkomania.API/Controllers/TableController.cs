0ee78ebb67a9870b0279f024df43b974e3ea3958
kelostrada <kelostrada@gmail.com>
2016-03-19 04:05:35 +0100

    joining table

--- backend/Planszowkomania.API/Controllers/TableController.cs
+++ backend/Planszowkomania.API/Controllers/TableController.cs
@@ -9,11 +9,18 @@ using Microsoft.AspNet.Identity;
 using Planszowkomania.API.Models.Entities;
 using Planszowkomania.API.Models.Front;
 using Planszowkomania.API.Models.Results;
+using Planszowkomania.API.Services;
 
 namespace Planszowkomania.API.Controllers
 {
     public class TableController : ControllerBase
     {
+        private readonly TableService _tableService;
+        public TableController()
+        {
+            _tableService = new TableService();
+        }
+
         [HttpGet]
         public IHttpActionResult All()
         {
@@ -73,12 +80,14 @@ namespace Planszowkomania.API.Controllers
 
             var table = context.Tables.Find(tableJoinModel.TableId);
 
-            if (table == null)
+            try
             {
-                return BadRequest("Wrong tableId");
+                _tableService.Join(user, table);
             }
-
-
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
             return Ok();
         }
---
+++ backend/Planszowkomania.API/Services/TableService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Planszowkomania.API.Models.Entities;
+
+namespace Planszowkomania.API.Services
+{
+    public class TableService
+    {
+        private readonly AppDbContext _context;
+
+        public TableService()
+        {
+            _context = new AppDbContext();
+        }
+
+        public void Join(User user, Table table)
+        {
+            if (table == null)
+            {
+                throw new Exception("Wrong tableId");
+            }
+
+            if (table.Participations.Any(p => p.Participant.Id == user.Id))
+            {
+                throw new Exception("Already joined");
+            }
+
+            var participation = new Participation
+            {
+                Participant = user,
+                Status = AcceptanceStatus.Pending,
+                Table = table
+            };
+            _context.Participations.Add(participation);
+            _context.SaveChanges();
+        }
+    }
+}
