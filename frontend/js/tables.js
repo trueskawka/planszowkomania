76f01cc7534ce94327ef947e12b6fef299b541c4
Alicja Raszkowska <malavarena@gmail.com>
2016-03-19 16:55:04 +0100

    accept invite

--- frontend/js/tables.js
+++ frontend/js/tables.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   var tid = tableid;
   tableid = "api/table/details/" + tableid;
 
-  authorizedGet(tableid, function(data){
-    console.log(data);
+  setupSignupButtons(tid);
 
+  authorizedGet(tableid, function(data){
     $("#gameDesc").text(data.game.description);
     $("#gameName").text(data.game.name);
     $("#gameImg").attr("src", "../files/images/" + data.game.image);
@@ -68,4 +68,54 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+var setupSignupButtons = function(tableid) {
+
+  $("#pendingInv").on('click', '.btn-success', function(e) {
+    var or = $(e.currentTarget);
+    var parent = $(or).parents('.player')[0];
+    console.log(tableid);
+
+    var data = {
+      "userId": $(parent).data("playerid"),
+      "tableid": tableid
+    }
+
+    $(parent).remove();
+
+    authorizedPost("api/table/accept", data, function(){
+      getUserDetails(function(data) {
+          var html = "";
+          html += "<div class='player' data-playerid='" + data.id + "'>";
+          html += '<div class="text"><img class="playerImg" src="../files/images/' + data.image + '" />';
+          html += '<h4>'+data.userName + '</h4>';
+          html += '<div class="buttons"><button class="btn btn-raised btn-warning">Odrzuć</button><button class="btn btn-raised btn-success">Zaakceptuj</button></div>';
+          html += '</div></div>';
+
+          $("#acceptedInv").append(html);
+        });
+    });
+
+  $("#pendingInv").on('click', '.btn-warning', function(e) {
+    var gr = $(e.currentTarget);
+    var parent = $(gr).parents('.player')[0];
+
+    tableid = "api/table/join/" + tableid;
+
+    var data = {
+      "userId": $(parent).data("playerid"),
+      "tableid": tableid
+    }
+
+    authorizedPost("api/table/reject", data, function(){
+      getUserDetails(function(data) {
+        $(parent).remove();
+        });
+    });
+
+  });
+
+});
 }
